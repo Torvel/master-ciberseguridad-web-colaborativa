ad7239f92782b6c14714e28594a201caeadc7128
FKShield <FKShield@users.noreply.github.com>
2026-04-27 19:44:08 +0200

    Fix: Vulnerabilidad crítica de acceso no autorizado en removeAllUsers

--- app/controllers/Api.java
+++ app/controllers/Api.java
@@ -2,12 +2,22 @@ package controllers;
 
 
 import com.google.gson.JsonObject;
+import models.Constants;
 import models.User;
 import play.mvc.Controller;
 
 public class Api extends Controller {
 
     public static void removeAllUsers(){
+        if (!session.contains("username")) {
+            forbidden();
+        }
+        
+        User u = User.loadUser(session.get("username"));
+        if (u == null || !u.getType().equals(Constants.User.TEACHER)) {
+            forbidden();
+        }
+
         User.removeAll();
         renderJSON(new JsonObject());
     }
