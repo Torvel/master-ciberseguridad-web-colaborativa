284cf24cb0e41757fe1d523c3e71ef27ab02d987
AnimeEnjoyer01 <lorenzo.porras.duque@gmail.com>
2026-04-27 16:21:08 -0500

    Refactor getMd5
    Arreglo de MD5

--- app/helpers/HashUtils.java
+++ app/helpers/HashUtils.java
@@ -1,23 +1,27 @@
 package helpers;
 
-
 import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.nio.charset.StandardCharsets;
 
 public class HashUtils {
 
     public static String getMd5(String s){
         try {
             MessageDigest m = MessageDigest.getInstance("MD5");
-            m.reset();
-            m.update(s.getBytes());
-            byte[] digest = m.digest();
-            BigInteger bigInt = new BigInteger(1,digest);
+            byte[] digest = m.digest(s.getBytes(StandardCharsets.UTF_8));
+
+            BigInteger bigInt = new BigInteger(1, digest);
             String hashtext = bigInt.toString(16);
-            while(hashtext.length() < 32){
+
+            while (hashtext.length() < 32) {
                 hashtext = "0" + hashtext;
             }
-        } catch (Exception e) {}
-        return s;
+
+            return hashtext;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
